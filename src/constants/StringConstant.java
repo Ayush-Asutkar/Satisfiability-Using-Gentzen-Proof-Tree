414568f1693c3d695ae41ac8351535444560acf1
Ayush-Asutkar <asutkarayush@gmail.com>
2023-09-24 14:17:36 +0530

    Added String constants

---
+++ src/constants/StringConstant.java
@@ -0,0 +1,9 @@
+package constants;
+
+public interface StringConstant {
+    String IMPLICATION = String.valueOf('\u2192');
+    String DOUBLE_IMPLICATION = String.valueOf('\u2194');
+    String DISJUNCTION = String.valueOf('\u22C0');
+    String CONJUNCTION = String.valueOf('\u22C1');
+    String NOT = String.valueOf('\u223C');
+}
