dddff3cb3c99fa6c8a02468bf49354e329371558
Ayush-Asutkar <asutkarayush@gmail.com>
2023-09-24 15:54:06 +0530

    Added splitting index finder

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,9 +1,8 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import constants.StringOperators;
+
+import java.util.*;
 
 public class Node {
     private final Set<List<String>> leftHandSide;
@@ -28,6 +27,47 @@ public class Node {
 
     public List<Node> getNewNodes() {
         return null;
+    }
+
+    /**
+     * This function will return the index at which splitting should occur
+     * @param formula a list of string which is a particular formula
+     * @return index of splitting character, -1 if can not be splitted
+     */
+    private static int splittingCharacterIndex(List<String> formula) {
+        //converting to postfix, and return the index of the last operator
+        Stack<Integer> stack = new Stack<>();
+
+        int result = -1;
+
+        for (int i=0; i<formula.size(); i++) {
+            String symbol = formula.get(i);
+
+            //check if the symbol is an operator
+            if(StringOperators.isOperator(symbol)) {
+                while (!stack.isEmpty()  &&
+                        (StringOperators.precedenceOfOperator(symbol)
+                                <= StringOperators.precedenceOfOperator(formula.get(stack.peek())))) {
+                    result = stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            } else if(symbol.equals("(")) {
+                stack.push(i);
+            } else if(symbol.equals(")")) {
+                while(!stack.isEmpty()  &&  !formula.get(stack.peek()).equals("(")) {
+                    result = stack.peek();
+                    stack.pop();
+                }
+                stack.pop();
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result = stack.pop();
+        }
+
+        return result;
     }
 
     public boolean isLeafNode() {
@@ -67,24 +107,77 @@ public class Node {
 
     //for testing
     public static void main(String[] args) {
-        List<String> leftList1 = new ArrayList<>();
-        leftList1.add("Alice");
-        List<String> leftList2 = new ArrayList<>();
-        leftList2.add("Bob");
+//        List<String> list = new ArrayList<>();
+//        list.add("(");
+//        list.add("(");
+//        list.add("A");
+//        list.add(StringOperators.OR);
+//        list.add("P");
+//        list.add(")");
+//        list.add(StringOperators.AND);
+//        list.add("(");
+//        list.add("B");
+//        list.add(StringOperators.OR);
+//        list.add(StringOperators.NEGATION);
+//        list.add("P");
+//        list.add(")");
+//        list.add(")");
+//        list.add(StringOperators.IMPLICATION);
+//        list.add("(");
+//        list.add("A");
+//        list.add(StringOperators.OR);
+//        list.add("P");
+//        list.add(")");
+//        list.add(StringOperators.AND);
+//        list.add("(");
+//        list.add("B");
+//        list.add(StringOperators.OR);
+//        list.add(StringOperators.NEGATION);
+//        list.add("P");
+//        list.add(")");
 
-        List<String> rightList1 = new ArrayList<>();
-        rightList1.add("Bob");
+        List<String> list = new ArrayList<>();
+        list.add("(");
+        list.add("a");
+        list.add(StringOperators.IMPLICATION);
+        list.add("b");
+        list.add(")");
+        list.add(StringOperators.AND);
+        list.add("(");
+        list.add("b");
+        list.add(StringOperators.IMPLICATION);
+        list.add("a");
+        list.add(")");
 
-        Set<List<String>> right = new HashSet<>();
-        Set<List<String>> left = new HashSet<>();
+        System.out.println(list);
+        int result = splittingCharacterIndex(list);
+        System.out.println(result);
+        System.out.println(splittingCharacterIndex(list) + " : " + list.get(splittingCharacterIndex(list)));
 
-        left.add(leftList1);
-        left.add(leftList2);
-        right.add(rightList1);
+//        List<String> leftList1 = new ArrayList<>();
+//        leftList1.add("Alice");
+//        List<String> leftList2 = new ArrayList<>();
+//        leftList2.add("Bob");
+//
+//        List<String> rightList1 = new ArrayList<>();
+//        rightList1.add("Bob");
+//
+//        Set<List<String>> right = new HashSet<>();
+//        Set<List<String>> left = new HashSet<>();
+//
+//        left.add(leftList1);
+//        left.add(leftList2);
+//        right.add(rightList1);
+//
+//        Node node = new Node(left, right);
+//        System.out.println(node);
+//        System.out.println(node.isLeafNode());
+//        System.out.println(node.isContradiction());
 
-        Node node = new Node(left, right);
-        System.out.println(node);
-        System.out.println(node.isLeafNode());
-        System.out.println(node.isContradiction());
+//        System.out.println(StringOperators.CONJUNCTION);
+//        System.out.println(StringOperators.DISJUNCTION);
+//        System.out.println(StringOperators.DOUBLE_IMPLICATION);
+//        System.out.println(StringOperators.IMPLICATION);
+//        System.out.println(StringOperators.NOT);
     }
 }
